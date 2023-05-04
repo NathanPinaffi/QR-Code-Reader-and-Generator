d068e08847e55026a54734da2e5144e65e966ca7
NathanPinaffi <pinaffin905@gmail.com>
2023-05-04 13:16:06 +0000

    Removendo imagens

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const QRCode = require('qrcode');
 
 const prompt = require("prompt-sync")();
 
-const nameArchive = prompt("Digite o nome: ");
+const nameArchive = prompt("Digite o nome do arquivo: ");
 const message = prompt("Digite a mensagem: ");
 
 QRCode.toFile(`./img/${nameArchive}.png`, `${message}`, {
