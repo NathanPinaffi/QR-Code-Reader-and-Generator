2accfcd46fdf3362f85a07abae23f70ce7526a80
NathanPinaffi <pinaffin905@gmail.com>
2023-08-14 02:38:37 +0000

    Vários formatos de arquivo

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ const QRCode = require('qrcode');
 const prompt = require("prompt-sync")();
 
 const fileName = prompt("Digite o nome do arquivo: ");
+const fileExtension = prompt("Digite o formato do arquivo: ");
 const content = prompt("Digite a mensagem: ");
 
-QRCode.toFile(`./img/${fileName}.png`, `${content}`, {
-    errorCorrectionLevel: 'H'
+QRCode.toFile(`./img/${fileName}.${fileExtension}`, `${content}`, {
+    errorCorrectionLevel: 'H',
 }, function(err) {
     if(err) throw err;
     console.log('QR Code saved!');
